75e1f50a48b0761ab7c18fb076dee4dab16e89ff
Sean Kennedy <snkennedy21@gmail.com>
2022-07-27 12:18:11 -0400

    complete F4

--- receipts/admin.py
+++ receipts/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+admin.site.register(models.Account)
+admin.site.register(models.Receipt)
+admin.site.register(models.ExpenseCategory)
