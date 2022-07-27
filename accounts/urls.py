7ca36adc92e8798bc2a5807bd78f8d9748877952
Sean Kennedy <snkennedy21@gmail.com>
2022-07-27 15:17:32 -0400

    partial completion of F10

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,5 +4,6 @@ from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
   path("login/", LoginView.as_view(), name="login"),
-  path('logout/', LogoutView.as_view(), name="logout")
+  path('logout/', LogoutView.as_view(), name="logout"),
+  path('signup/', views.signup, name="signup")
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+from django.shortcuts import redirect
+from django.contrib.auth.models import User
 
-# Create your views here.
+def signup(request):
+  form = UserCreationForm()
+
+  if request.method == "POST":
+    username = request.POST.get('username')
+    password = request.POST.get('password1')
+
+    print(username)
+    print(password)
+    user = User.objects.create_user(username, f'{username}@email.com', password)
+
+    print(user)
+    if user is not None:
+      login(request, user)
+      return redirect('home')
+    else:
+      messages.error(request, "Username Or password does not exist")
+
+  context = {
+    "form": form
+  }
+
+  return render(request, 'registration/signup.html', context)
+
