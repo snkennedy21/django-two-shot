8ccffe4118fb889dfd9c6c1c27e905c0d48b181f
Sean Kennedy <snkennedy21@gmail.com>
2022-07-27 12:46:23 -0400

    complete F6

--- receipts/models.py
+++ receipts/models.py
@@ -21,6 +21,7 @@ class Receipt(models.Model):
   total = models.DecimalField(max_digits=10, decimal_places=3)
   tax = models.DecimalField(max_digits=10, decimal_places=3)
   date = models.DateTimeField(auto_now_add=True)
+  purchaser = models.ForeignKey(User, related_name="receipts", on_delete=models.CASCADE, null=True)
   category = models.ForeignKey("ExpenseCategory", related_name="receipts", on_delete=models.CASCADE)
   account = models.ForeignKey("Account", related_name="receipts", on_delete=models.CASCADE, null=True)
 
