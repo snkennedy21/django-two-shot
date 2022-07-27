522e854054eb71c802ec081afbc749a5c20d4f60
Sean Kennedy <snkennedy21@gmail.com>
2022-07-27 13:05:24 -0400

    complete F6

--- expenses/urls.py
+++ expenses/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
+from django.urls import reverse_lazy
 
 urlpatterns = [
+    path('', RedirectView.as_view(url=reverse_lazy('home'))),
     path('admin/', admin.site.urls),
     path('receipts/', include('receipts.urls'))
-
 ]
